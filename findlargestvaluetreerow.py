83a4567fa0bb1ffdc5da53a8d39143f68ae3ec39
Jacky <thejackychen@gmail.com>
2024-12-27 06:06:30 -0500

    tree stuff ez

---
+++ findlargestvaluetreerow.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import List
+from typing import Optional
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        #bfs approach
+        if not root:
+            return []
+        q = deque([root])
+        maxArr = []
+        while q:
+            row = []
+
+            for node in range(len(q)):
+                node = q.popleft()
+                row.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            
+            maxArr.append(max(row))
+        
+        return maxArr
+        
+
